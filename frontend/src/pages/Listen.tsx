e93a7c039c19fff238bf1b089fa8582559691dee
Manuj Mishra <mm4519@ic.ac.uk>
2022-02-05 20:39:58 +0000

    Sleep to emulate API call

---
+++ frontend/src/pages/Listen.tsx
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from "react-bootstrap/Container";
+import PrimaryContent from "../layout/PrimaryContent";
+
+function Home() {
+    return(
+        <PrimaryContent>
+            <h4>Finding a lobby ...</h4>
+        </PrimaryContent>
+    )
+}
+
+export default Home;
--- frontend/src/pages/Lobby.tsx
+++ frontend/src/pages/Lobby.tsx
@@ -2,13 +2,31 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from "react-bootstrap/Container";
 import PrimaryContent from "../layout/PrimaryContent";
+import {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
 
-function Home() {
+
+function Lobby() {
+    const navigate = useNavigate();
+    const [lobbyFound, setLobbyFound] = useState(false);
+    useEffect(
+        () => {
+            let dummyTimer = setTimeout(() => setLobbyFound(true), 3 * 1000);
+            return () => {
+                clearTimeout(dummyTimer);
+            };
+        },
+        []
+    );
     return(
         <PrimaryContent>
-            <h4>Finding a lobby ...</h4>
+            {
+                lobbyFound
+            ? navigate("/listen")
+            : <h4>Finding a lobby ...</h4>
+            }
         </PrimaryContent>
     )
 }
 
-export default Home;
+export default Lobby;
