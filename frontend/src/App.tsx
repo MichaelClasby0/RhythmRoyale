d8824926c54b9866b7c1b7b3ac688e88a5ca79af
Manuj Mishra <mm4519@ic.ac.uk>
2022-02-05 20:56:41 +0000

    Dummy play page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Lobby from "./pages/Lobby";
+import Listen from "./pages/Listen";
+import Play from "./pages/Play";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/lobby" element={<Lobby/>}/>
+            <Route path="/listen" element={<Listen/>}/>
+            <Route path="/play" element={<Play/>}/>
         </Routes>
     );
 }
