cc19d567b9958886717f86a1fc489a9dcda0a827
Michael Clasby <michaelclasby@hotmail.com>
2022-02-06 00:45:27 +0000

    Fix literally everything

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = require("http").createServer(app);
+
+app.use(express.json());
+app.use(express.static("../frontend/build"));
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
@@ -21,18 +29,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
-app.get("/",  (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-})
 
 io.on("connection", function (socket: any) {
   const newGameId = Math.random().toString(20).slice(2, 6);
 
 });
 
-app.use(express.json());
-app.use(express.static("../frontend/build"));
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
 
