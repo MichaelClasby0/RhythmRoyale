a8eeb88893d3a50f50969ed15196607b006433ce
Manuj Mishra <mm4519@ic.ac.uk>
2022-02-06 09:30:20 +0000

    Replay animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,20 @@ import Home from "./pages/Home";
 import Game from "./pages/Game";
 import Listen from "./pages/Listen";
 import Record from "./pages/Record";
-import Visualisation from "./components/Visualisation";
+import {Anim, Vis} from "./components/Visualisation";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Visualisation />} />
+        <Route path="/test" element={<Anim original={[
+            { type: "beat", duration: 600 },
+            { type: "gap", duration: 400 },
+            { type: "beat", duration: 500 },
+            { type: "gap", duration: 300 },
+            { type: "beat", duration: 1200 },
+        ]} />} />
         <Route path="/game/:gameId" element={<Game />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/components/Visualisation.tsx
+++ frontend/src/components/Visualisation.tsx
@@ -4,6 +4,7 @@ import {useState, useEffect} from "react";
 import useKeypressBeats from "../hooks/useKeyPress";
 
 function Vis() {
+    // @ts-ignore
     const isDown = useKeypressBeats(" ")[0]
     const time_limit = 3 * 1000
     const [circles, setCircles] = useState<Array<number>>([])
@@ -23,7 +24,7 @@ function Vis() {
             setTime(time + 2)
         }, 10);
         return () => clearInterval(interval);
-    }, [time]);
+    }, [time, circles]);
 
     return (
         <PrimaryContent>
@@ -38,5 +39,50 @@ function Vis() {
     );
 }
 
-//
-export default Vis;
+
+// @ts-ignore
+function Anim({original}) {
+    const time_limit = 3 * 1000
+    const [beats, setBeats] = useState(original)
+    const [circles, setCircles] = useState<Array<number>>([])
+    const [time, setTime] = useState(75);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+
+            if (time * 5 > time_limit) {
+                clearInterval(interval);
+                return;
+            }
+
+            const curr = beats[0]
+            if (curr.type === "beat") {
+                setCircles([...circles, time])
+            }
+            if (curr.duration < 10) {
+                setBeats(beats.slice(1))
+            } else {
+                setBeats([{type: curr.type, duration: curr.duration - 10}, ...beats.slice(1)])
+            }
+            setTime(time + 2)
+        }, 10);
+        return () => clearInterval(interval);
+    }, [time, circles, beats]);
+
+    return (
+        <PrimaryContent>
+            <div className="rect">
+                {
+                    circles.map((circle, index) => {
+                        return <span className="circle" style={{left: circle.toString() + "px"}} key={index}/>
+                    })
+                }
+            </div>
+        </PrimaryContent>
+    );
+}
+
+
+
+
+export {Vis, Anim};
