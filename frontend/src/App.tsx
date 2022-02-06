4efe1589bd882df18f4653ca3f8df829bf4f79b6
Michael Clasby <michaelclasby@hotmail.com>
2022-02-06 04:38:10 +0000

    Fix merge anchor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import React, {useMemo} from "react";
-import {Routes, Route} from "react-router-dom";
-import {io} from "socket.io-client";
-import {SocketContext} from "./context/socket";
-import Home from "./pages/Home";
-import Lobby from "./pages/Lobby";
-import Listen from "./pages/Listen";
-import Record from "./pages/Record";
-
-function App() {
-    const socket = useMemo(() => io("http://localhost:5000"), []);
-
-    return (
-        <SocketContext.Provider value={socket}>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/lobby" element={<Lobby/>}/>
-                <Route path="/listen" element={<Listen/>}/>
-                <Route path="/record" element={<Record/>}/>
-            </Routes>
-        </SocketContext.Provider>
-    );
-=======
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
@@ -35,7 +11,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
